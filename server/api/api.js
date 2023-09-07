c5c1ceff85770b5d5a4170d5a3606eb4f90219bd
EdvardasNavickas <nps.anapus@gmail.com>
2023-09-07 14:53:17 +0300

    Final commit

--- server/api/api.js
+++ server/api/api.js
@@ -1,4 +1,6 @@
+import path from 'path';
 import express from 'express';
+import multer from 'multer';
 import { register } from './register.js';
 import { login } from './login.js';
 import { movies } from './movies.js';
@@ -7,6 +9,27 @@ const api = express.Router();
 
 api.get('/', (req, res) => {
     return res.send('API ERROR: nepilnas URL');
+});
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'cover_' + file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    },
+});
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1e7,
+    }
+});
+api.use('/upload', upload.single('image_file'), (req, res) => {
+    return res.json({
+        msg: 'Upload complete...',
+        path: req.file.filename,
+    });
 });
 
 api.use('/register', register);
